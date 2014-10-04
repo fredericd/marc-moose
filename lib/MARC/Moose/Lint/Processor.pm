71ca778e8f04fda5549bc75d1d2958f2b0d92eb2
Frédéric Demians <f.demians@tamil.fr>
2014-10-04 16:03:19 +0200

    Lint: generalize checker

--- lib/MARC/Moose/Lint/Processor.pm
+++ lib/MARC/Moose/Lint/Processor.pm
@@ -19,10 +19,33 @@ has lint => (is => 'rw', isa => 'MARC::Moose::Lint::Checker');
 
 =attr file
 
-The name of the ISO2709 file containing biblio records to control.
+The name of an ISO2709 file containing biblio records to control. When the
+processor object is created with this attribute, other attributes are
+automatically constructed: C<reader> as L<MARC::Moose::Reader::File::Iso2709>
+object reading the file, C<writer_ok> and C<writer_bad> as
+L<MARC::Moose::Writer> object with an ISO2709 formater writing to files named
+'file.ok' and 'file.bad', and C<fh_log> as a L<IO::File>, writing a text file
+named 'file.log'.
 
 =cut
-has file => (is => 'rw' );
+has file => (
+    is => 'ro',
+    isa => 'Str',
+    trigger => sub {
+        my ($self, $file) = @_;
+        $self->reader( MARC::Moose::Reader::File::Iso2709->new(
+            file => $file ) );
+        $self->writer_ok( MARC::Moose::Writer->new(
+            formater => MARC::Moose::Formater::Iso2709->new(),
+            fh => IO::File->new("$file.ok", ">:encoding(utf8)")
+        ) );
+        $self->writer_bad( MARC::Moose::Writer->new(
+            formater => MARC::Moose::Formater::Iso2709->new(),
+            fh => IO::File->new("$file.bad", ">:encoding(utf8)")
+        ) );
+        $self->fh_log( IO::File->new("$file.log", ">:encoding(utf8)") );
+    },
+);
 
 
 =attr cleaner
@@ -35,31 +58,41 @@ records before validating them.
 has cleaner => (is => 'rw', isa => 'MARC::Moose::Formater');
 
 
-has reader => (is => 'rw');
-has writer_ok => (is => 'rw');
+=attr reader
+
+A L<MARC::Moose::Reader> object from which biblio records are read.
+
+=cut
+has reader => (is => 'rw', isa => 'MARC::Moose::Reader' );
+
+
+=attr writer_ok
+
+A L<MARC::Moose::Writer> object in which valid biblio records are written.
+
+=cut
+has writer_ok => (is => 'rw', isa => 'MARC::Moose::Writer' );
+
+
+=attr writer_bad
+
+A L<MARC::Moose::Writer> object in which unvalid biblio records are written.
+
+=cut
+has writer_bad => (is => 'rw', isa => 'MARC::Moose::Writer' );
+
+
+=attr writer_bad
+
+A L<IO::File> file handle which is used to write unvalid biblio records with
+generated warnings.
+
+=cut
+has fh_log => (is => 'rw', isa => 'IO::File');
+
 has count_ok => (is => 'rw', isa => 'Int', default => 0);
-has writer_bad => (is => 'rw');
 has count_bad => (is => 'rw', isa => 'Int', default => 0);
-has fh_log => (is => 'rw');
 
-
-
-sub BUILD {
-    my $self = shift;
-
-    my $file = $self->file;
-    $self->reader( MARC::Moose::Reader::File::Iso2709->new(
-        file => $file ) );
-    $self->writer_ok( MARC::Moose::Writer->new(
-        formater => MARC::Moose::Formater::Iso2709->new(),
-        fh => IO::File->new("$file.ok", ">:encoding(utf8)")
-    ) );
-    $self->writer_bad( MARC::Moose::Writer->new(
-        formater => MARC::Moose::Formater::Iso2709->new(),
-        fh => IO::File->new("$file.bad", ">:encoding(utf8)")
-    ) );
-    $self->fh_log( IO::File->new("$file.log", ">:encoding(utf8)") );
-};
 
 
 sub process {
@@ -90,10 +123,12 @@ sub start_message {
     say "Start linting biblio records";
 }
 
+
 sub process_message {
     my $self = shift;
     say $self->count, " - OK ", $self->count_ok, " - BAD ", $self->count_bad;
 }
+
 
 sub end_message {
     my $self = shift;
@@ -128,7 +163,7 @@ __PACKAGE__->meta->make_immutable;
  package Main;
  
  my $processor = MARC::Moose::Lint::Processor->new(
-     lint => MARC::Moose::Lint::Checker->new( file => 'unimarc.rules',
+     lint => MARC::Moose::Lint::Checker::RulesFile->new( file => 'unimarc.rules',
      file => 'biblio.mrc',
      cleaner => PeterCleaner->new(),
      verbose => 1,
@@ -141,6 +176,24 @@ an ISO2709 containing biblio records complying to the rules, (2)
 C<biblio.mrc.bad> containing biblios violating the rules, and (3)
 C<biblio.mrc.log> containing a textual representation of biblio records
 violating the rules + a description of violated rules.
+
+A more specific construction is also possible:
+
+ my $processor = MARC::Moose::Lint::Processor->new(
+     lint => MARC::Moose::Lint::Checker::RulesFile->new( file => 'unimarc.rules',
+     reader => MARC::Moose::Reader::File::Marcxml->new('biblio.xml'),
+     writer_ok => MARC::Moose::Writer->new(
+         formater => MARC::Moose::Formater::Marcxml->new(),
+         fh => IO::File->new('ok.xml', '>:encoding(utf8')
+     ),
+     writer_bad => MARC::Moose::Writer->new(
+         formater => MARC::Moose::Formater::Marcxml->new(),
+         fh => IO::File->new('bad.xml', '>:encoding(utf8')
+     ),
+     fh_log => IO::File->new('warnings.log', '>:encoding(utf8'),
+     verbose => 1,
+ };
+ $processor->run();
 
 
 =head1 SEE ALSO
